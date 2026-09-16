f61e460efacd9a672adcdbd5047f5aabef3de83f
Darren Oakey <darren@insidemind.com.au>
2026-09-16 22:02:16 +1000

    gl/ios27-scene: Point UI-test simulator at iPhone 18 Pro (iOS 27.0)
    - Point UI-test simulator at iPhone 18 Pro (iOS 27.0)
    - Fix high-severity @xmldom/xmldom advisory (transitive via @capacitor/cli)
    - Adopt UIScene lifecycle to fix iOS 27 launch crash; bump to 1.0.4
    
    Via: greenline gate

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -33,6 +33,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: "Default Configuration",
+                                          sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
+    }
+
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return .portrait
     }
